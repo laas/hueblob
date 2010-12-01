a24a468fd253b29b8930215b88d5186a165da586
Thomas Moulard <thomas.moulard@gmail.com>
2010-12-01 19:09:24 +0100

    Clean and document the fake_camera_synchronized_node.

--- src/fake_camera_synchronizer_node.cpp
+++ src/fake_camera_synchronizer_node.cpp
@@ -1,21 +1,74 @@
 #include <boost/bind.hpp>
 
 #include <ros/console.h>
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
 #include <ros/param.h>
-#include <std_msgs/String.h>
-#include <sensor_msgs/Image.h>
+#include <ros/ros.h>
+
+#include <image_transport/image_transport.h>
 
 #include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
 
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/Image.h>
+
+// This node takes care of resynchronizing images and camera
+// information to enforce time stamp matching.
+// The main interest of this node is being able to realize stereo
+// image processing when left and right camera image acquisition
+// are not synchronized. This is required for Firewire cameras
+// when using camera1394 (in CBoxTurtle release) or usb_cam.
+//
+// Here is how the can be used, it makes the assumption that a
+// stereo camera pair topics are setup in the /stereo prefix and
+// that you want to get a synchronized stereo pair in /stereo_sync.
+//
+// Input:
+// - /stereo/left/image_raw
+// - /stereo/left/camera_info
+// - /stereo/right/image_raw
+// - /stereo/right/camera_info
+//
+// Output:
+// - /stereo_sync/left/image_raw
+// - /stereo_sync/left/camera_info
+// - /stereo_sync/right/image_raw
+// - /stereo_sync/right/camera_info
+//
+// Launch the following command to start the synchronization mode:
+// rosrun hueblob fake_camera_synchronizer_node _in:=/stereo _out:=/stereo_sync
+//
+// Then the stereo image processing node can be started:
+// ROS_NAMESPACE=/stereo_sync rosrun stereo_image_proc stereo_image_proc
+
+
+/// \brief Define policy type for Synchronizer filter.
+///
+/// This will matches image and camera information for
+/// both left and right cameras.
 typedef message_filters::sync_policies::ApproximateTime<
   sensor_msgs::Image, sensor_msgs::CameraInfo,
   sensor_msgs::Image, sensor_msgs::CameraInfo>
 policy_t;
 
+
+/// \brief Main callback which takes care of republishing.
+///
+/// This callback is called by the Synchronizer filter when
+/// messages for both left/right images and cameras information
+/// have been collected.
+///
+/// The callback the left image timestamps in all the other messages.
+///
+/// \param pub_l left image publisher (binded)
+/// \param pub_cam_l left camera information publisher (binded)
+/// \param pub_r right image publisher (binded)
+/// \param pub_cam_r right camera information publisher (binded)
+/// \param left left image
+/// \param left_camera left camera information
+/// \param right right image
+/// \param right_camera right camera information
 void imageCallback(image_transport::Publisher& pub_l,
 		   ros::Publisher& pub_cam_l,
 		   image_transport::Publisher& pub_r,
@@ -44,14 +97,19 @@ void imageCallback(image_transport::Publisher& pub_l,
   pub_cam_r.publish(right_camera_);
 }
 
+
+/// \brief Main entry point.
 int main(int argc, char **argv)
 {
+  // ROS initialization.
   ros::init(argc, argv, "fake_camera_synchronizer");
 
+  // Parameters definition.
   std::string in, out;
   ros::param::param<std::string>("~in", in, "");
   ros::param::param<std::string>("~out", out, "");
 
+  // Topic name construction.
   std::string left_in = in + "/left/image_raw";
   std::string left_cam_in = in + "/left/camera_info";
   std::string left_out = out + "/left/image_raw";
@@ -62,9 +120,11 @@ int main(int argc, char **argv)
   std::string right_out = out + "/right/image_raw";
   std::string right_cam_out = out + "/right/camera_info";
 
+  // Node initialization.
   ros::NodeHandle n("fake_camera_synchronizer");
   image_transport::ImageTransport it(n);
 
+  // Publishers creation.
   image_transport::Publisher pub_l = it.advertise(left_out, 1);
   image_transport::Publisher pub_r = it.advertise(right_out, 1);
   ros::Publisher pub_cam_l =
@@ -72,6 +132,7 @@ int main(int argc, char **argv)
   ros::Publisher pub_cam_r =
     n.advertise<sensor_msgs::CameraInfo>(right_cam_out, 1);
 
+  // Subscribers creation.
   message_filters::Subscriber<sensor_msgs::Image>
     sub_l(n, left_in, 1);
   message_filters::Subscriber<sensor_msgs::CameraInfo>
@@ -81,7 +142,7 @@ int main(int argc, char **argv)
   message_filters::Subscriber<sensor_msgs::CameraInfo>
     sub_r_cam(n, right_cam_in, 1);
 
-
+  // Message filter creation.
   message_filters::Synchronizer<policy_t> sync
     (policy_t(10), sub_l, sub_l_cam, sub_r, sub_r_cam);
   sync.registerCallback
@@ -90,11 +151,5 @@ int main(int argc, char **argv)
 		 boost::ref(pub_r), boost::ref(pub_cam_r),
 		 _1, _2, _3, _4));
 
-  ros::Rate loop_rate(10);
-
-  while (ros::ok())
-    {
-      ros::spinOnce();
-      loop_rate.sleep();
-    }
+  ros::spin();
 }
