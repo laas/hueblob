7c590ac7694fec26149a500155841390ae829714
Thomas Moulard <thomas.moulard@gmail.com>
2010-12-06 14:27:08 +0100

    Factorize tracking code in test case.

--- tests/object.cpp
+++ tests/object.cpp
@@ -6,6 +6,40 @@
 
 #include "libhueblob/object.hh"
 
+void trackObject(const std::string& viewFilename,
+		 const std::string& frameFilename)
+{
+  Object object;
+  cv::Mat view = cv::imread(viewFilename);
+  cv::Mat image = cv::imread(frameFilename);
+  object.addView(view);
+
+  EXPECT_EQ(object.modelHistogram.size(), 1u);
+
+  boost::optional<cv::RotatedRect> rrect = object.track(image);
+
+  // Check that tracking succeed.
+  EXPECT_TRUE(rrect);
+
+  cv::Rect rect = rrect->boundingRect();
+
+  boost::format fmt("Blob location: %dx%d+%dx%d");
+  fmt % rect.x % rect.y % rect.width % rect.height;
+
+  std::cout << fmt << std::endl;
+
+  cv::Point top_left(rect.x, rect.y);
+  cv::Point bottom_right(rect.x + rect.width, rect.y + rect.height);
+  cv::rectangle(image, top_left, bottom_right,
+		cv::Scalar(0, 0, 255, 255), CV_FILLED);
+
+  boost::format filename("tracking_result_%s.png");
+  filename % frameFilename;
+
+  if(!cv::imwrite(filename.str(), image))
+    std::cerr << "Failed to save image.";
+}
+
 void viewHistogram(const cv::MatND& hist, const std::string& filename)
 {
   int hbins = Object::h_bins, sbins = Object::s_bins;
@@ -66,32 +100,7 @@ TEST(TestSuite, add_view)
 
 TEST(TestSuite, track)
 {
-  Object object;
-  cv::Mat view = cvLoadImage("./data/models/door.png");
-  cv::Mat image = cvLoadImage("./data/frames/door-frame.png");
-  object.addView(view);
-
-  EXPECT_EQ(object.modelHistogram.size(), 1u);
-
-  boost::optional<cv::RotatedRect> rrect = object.track(image);
-
-  // Check that tracking succeed.
-  EXPECT_TRUE(rrect);
-
-  cv::Rect rect = rrect->boundingRect();
-
-  boost::format fmt("Blob location: %dx%d+%dx%d");
-  fmt % rect.x % rect.y % rect.width % rect.height;
-
-  std::cout << fmt << std::endl;
-
-  cv::Point top_left(rect.x, rect.y);
-  cv::Point bottom_right(rect.x + rect.width, rect.y + rect.height);
-  cv::rectangle(image, top_left, bottom_right,
-		cv::Scalar(0, 0, 255, 255), CV_FILLED);
-
-  if(!cv::imwrite("tracking_result.png", image))
-    std::cerr << "Failed to save image.";
+  trackObject("./data/models/door.png", "./data/frames/door-frame.png");
 }
 
 int main(int argc, char **argv)
