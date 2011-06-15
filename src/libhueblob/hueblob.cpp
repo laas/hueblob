944b7c57259e2223c114341310fbcb89c5bf1fbb
Duong Dang <dang.duong@gmail.com>
2011-06-15 13:44:52 +0000

    display only object name in images

--- src/libhueblob/hueblob.cpp
+++ src/libhueblob/hueblob.cpp
@@ -138,20 +138,22 @@ HueBlob::spin()
           //                  << " " << width << " " << height);
           cv::rectangle(img, p1, p2, color, 1);
           stringstream ss (stringstream::in | stringstream::out);
-          boost::format fmter("[%3.3f %3.3f %3.3f %1.2f]");
-          (fmter % (*iter).cloud_centroid.transform.translation.x
-           %  (*iter).cloud_centroid.transform.translation.y
-           %  (*iter).cloud_centroid.transform.translation.z
-           %  (*iter).depth_density
-           );
-          cv::putText(img, fmter.str(), p1, CV_FONT_HERSHEY_SIMPLEX, 0.5, color);
+	  cv::putText(img, (*iter).name, p1, CV_FONT_HERSHEY_SIMPLEX, 
+		      0.5, color);
+          // boost::format fmter("[%3.3f %3.3f %3.3f %1.2f]");
+          // (fmter % (*iter).cloud_centroid.transform.translation.x
+          //  %  (*iter).cloud_centroid.transform.translation.y
+          //  %  (*iter).cloud_centroid.transform.translation.z
+          //  %  (*iter).depth_density
+          //  );
+          // cv::putText(img, fmter.str(), p1, CV_FONT_HERSHEY_SIMPLEX, 0.5, color);
 
-          boost::format fmter2("[%3.3f %3.3f %3.3f]");
-          (fmter2 % (*iter).position.transform.translation.x
-           %  (*iter).position.transform.translation.y
-           %  (*iter).position.transform.translation.z
-           );
-          cv::putText(img, fmter2.str(), pc, CV_FONT_HERSHEY_SIMPLEX, 0.5, color);
+          // boost::format fmter2("[%3.3f %3.3f %3.3f]");
+          // (fmter2 % (*iter).position.transform.translation.x
+          //  %  (*iter).position.transform.translation.y
+          //  %  (*iter).position.transform.translation.z
+          //  );
+          // cv::putText(img, fmter2.str(), pc, CV_FONT_HERSHEY_SIMPLEX, 0.5, color);
         }
 
       if (leftImage_){
